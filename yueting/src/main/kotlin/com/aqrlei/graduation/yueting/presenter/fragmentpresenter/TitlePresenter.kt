ba181900f467e697cea158e41d4c01da6809e2da
AqrLei <aqrdeveloper@gmail.com>
2018-04-27 23:50:32 +0800

    develop

--- yueting/src/main/kotlin/com/aqrlei/graduation/yueting/presenter/fragmentpresenter/TitlePresenter.kt
+++ yueting/src/main/kotlin/com/aqrlei/graduation/yueting/presenter/fragmentpresenter/TitlePresenter.kt
@@ -1,8 +1,8 @@
 package com.aqrlei.graduation.yueting.presenter.fragmentpresenter
 
-import android.database.Cursor
 import com.aqrairsigns.aqrleilib.basemvp.MvpContract
 import com.aqrairsigns.aqrleilib.util.DBManager
+import com.aqrairsigns.aqrleilib.util.DateFormatUtil
 import com.aqrlei.graduation.yueting.constant.DataConstant
 import com.aqrlei.graduation.yueting.ui.fragment.TitleFragment
 import io.reactivex.Observable
@@ -17,22 +17,62 @@ import io.reactivex.schedulers.Schedulers
 class TitlePresenter(mMvpView: TitleFragment) :
         MvpContract.FragmentPresenter<TitleFragment>(mMvpView) {
     companion object {
-        fun selectTypeObservable(type: String): Observable<Cursor?> {
+        fun selectTypeObservable(type: String): Observable<ArrayList<String>> {
             return Observable.defer {
+                val typeList = ArrayList<String>()
+
                 val c = DBManager.sqlData(
                         DBManager.SqlFormat.selectSqlFormat(
                                 DataConstant.TYPE_TABLE_NAME,
                                 DataConstant.TYPE_TABLE_C0_NAME,
                                 DataConstant.TYPE_TABLE_C1_TYPE,
-                                "="
-                        ),
-                        null, arrayOf(type), DBManager.SqlType.SELECT
-                )
-                        .getCursor()
-                Observable.just(c)
+                                "="),
+                        null, arrayOf(type), DBManager.SqlType.SELECT)
+                        .getCursor()?.apply {
+                            while (moveToNext()) {
+                                val temp = getString(getColumnIndex(DataConstant.TYPE_TABLE_C0_NAME))
+                                typeList.add(temp)
+                            }
+                        }
+                Observable.just(typeList)
             }
         }
 
+        fun addDataToDB(type: String, name: String): Observable<Boolean> {
+            return Observable.defer {
+                val dateTime = DateFormatUtil.simpleDateFormat(System.currentTimeMillis())
+                DBManager.sqlData(
+                        DBManager.SqlFormat.insertSqlFormat(
+                                DataConstant.TYPE_TABLE_NAME,
+                                arrayOf(DataConstant.TYPE_TABLE_C0_NAME,
+                                        DataConstant.TYPE_TABLE_C1_TYPE,
+                                        DataConstant.COMMON_COLUMN_CREATE_TIME)),
+                        arrayOf(name, type, dateTime),
+                        null,
+                        DBManager.SqlType.INSERT
+                )
+                Observable.just(DBManager.finish())
+            }
+        }
+
+    }
+
+    fun addData(type: String, name: String) {
+        val disposable = CompositeDisposable().apply {
+            add(addDataToDB(type, name)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeWith(object : DisposableObserver<Boolean>() {
+                        override fun onComplete() {}
+                        override fun onError(e: Throwable) {}
+                        override fun onNext(boolean: Boolean) {
+                            mMvpView.addFinish(boolean)
+                        }
+                    })
+            )
+
+        }
+        addDisposables(disposable)
     }
 
     fun getTypeInfoFromDB(type: String) {
@@ -40,16 +80,11 @@ class TitlePresenter(mMvpView: TitleFragment) :
             add(selectTypeObservable(type)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeWith(object : DisposableObserver<Cursor>() {
+                    .subscribeWith(object : DisposableObserver<ArrayList<String>>() {
                         override fun onComplete() {}
                         override fun onError(e: Throwable) {}
-                        override fun onNext(t: Cursor) {
-                            val typeList = ArrayList<String>()
-                            while (t.moveToNext()) {
-                                val temp = t.getString(t.getColumnIndex(DataConstant.TYPE_TABLE_C0_NAME))
-                                typeList.add(temp)
-                            }
-                            mMvpView.setTitleList(typeList)
+                        override fun onNext(data: ArrayList<String>) {
+                            mMvpView.setTitleList(data)
                         }
                     })
             )
--- yueting/src/main/kotlin/com/aqrlei/graduation/yueting/ui/fragment/TitleFragment.kt
+++ yueting/src/main/kotlin/com/aqrlei/graduation/yueting/ui/fragment/TitleFragment.kt
@@ -1,16 +1,24 @@
 package com.aqrlei.graduation.yueting.ui.fragment
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.AdapterView
+import android.widget.EditText
+import android.widget.TextView
 import com.aqrairsigns.aqrleilib.basemvp.MvpContract
+import com.aqrairsigns.aqrleilib.util.AppLog
+import com.aqrairsigns.aqrleilib.util.AppToast
 import com.aqrlei.graduation.yueting.R
 import com.aqrlei.graduation.yueting.constant.DataConstant
 import com.aqrlei.graduation.yueting.constant.YueTingConstant
 import com.aqrlei.graduation.yueting.presenter.fragmentpresenter.TitlePresenter
+import com.aqrlei.graduation.yueting.ui.FileActivity
 import com.aqrlei.graduation.yueting.ui.YueTingActivity
 import com.aqrlei.graduation.yueting.ui.YueTingListActivity
 import com.aqrlei.graduation.yueting.ui.adapter.YueTingListAdapter
+import com.aqrlei.graduation.yueting.ui.uiEt.createPopView
 import kotlinx.android.synthetic.main.main_title_fragment_list.*
 
 /**
@@ -19,7 +27,7 @@ import kotlinx.android.synthetic.main.main_title_fragment_list.*
 class TitleFragment : MvpContract.MvpFragment<TitlePresenter, YueTingListActivity>(),
         AdapterView.OnItemClickListener,
         AdapterView.OnItemLongClickListener,
-View.OnClickListener{
+        View.OnClickListener {
     companion object {
         fun newInstance(type: String): TitleFragment {
             return TitleFragment().apply {
@@ -34,6 +42,43 @@ View.OnClickListener{
         get() = TitlePresenter(this)
     override val layoutRes: Int
         get() = R.layout.main_title_fragment_list
+
+    private val bottomDialog: Dialog
+            by lazy {
+                createPopView(mContainerActivity, R.layout.manage_pop_view_list).apply {
+                    window.decorView?.apply {
+                        findViewById(R.id.newListTv)?.also {
+                            (it as TextView).text =
+                                    if (type == YueTingConstant.FRAGMENT_TITLE_TYPE_MUSIC) "新建歌单"
+                                    else "新建书单"
+                            it.setOnClickListener(this@TitleFragment)
+                        }
+                        findViewById(R.id.manageListsTv)
+                                ?.also {
+                                    (it as TextView).text =
+                                            if (type == YueTingConstant.FRAGMENT_TITLE_TYPE_MUSIC) "管理歌单"
+                                            else "管理书单"
+                                   it. setOnClickListener(this@TitleFragment)
+                                }
+                        findViewById(R.id.addItems)?.also {
+                            (it as TextView).text =
+                                    if (type == YueTingConstant.FRAGMENT_TITLE_TYPE_MUSIC) "添加歌曲"
+                                    else "添加书籍"
+                            it.setOnClickListener(this@TitleFragment)
+                        }
+                    }
+                }
+            }
+    private val newListDialog: Dialog
+            by lazy {
+                createPopView(mContainerActivity, R.layout.manage_new_list, Gravity.CENTER).apply {
+                    window.decorView?.apply {
+                        findViewById(R.id.sureTv).setOnClickListener(this@TitleFragment)
+                        findViewById(R.id.cancelTv).setOnClickListener(this@TitleFragment)
+                    }
+
+                }
+            }
 
     private val type: String
         get() = arguments.getString(YueTingConstant.FRAGMENT_TITLE_TYPE)
@@ -63,10 +108,41 @@ View.OnClickListener{
             }
 
     override fun onClick(v: View) {
-        if(v.id == R.id.listTitleTv ){
-            //TODO create list or manage list
+        when (v.id) {
+            R.id.listTitleTv -> {
+                bottomDialog.show()
+            }
+            R.id.manageListsTv -> {
+                AppLog.logDebug("AqrLei", "manageList")
+                bottomDialog.dismiss()
+            }
+            R.id.addItems -> {
+                FileActivity.jumpToFileActivity(
+                        mContainerActivity,
+                        0,
+                        type,
+                        titleList[0],
+                        YueTingConstant.YUE_TING_LIST_FILE)
+                bottomDialog.dismiss()
+            }
+            R.id.newListTv -> {
+                bottomDialog.dismiss()
+                newListDialog.show()
+            }
+            R.id.sureTv -> {
+                val name = (newListDialog.window.decorView
+                        .findViewById(R.id.listNameEt) as EditText)
+                        .text.toString()
+                verifyListTitle(name)
+            }
+            R.id.cancelTv -> {
+                newListDialog.dismiss()
+            }
         }
+
+
     }
+
     override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
         YueTingActivity.jumpToYueTingActivity(mContainerActivity, type, titleList[position])
@@ -82,12 +158,29 @@ View.OnClickListener{
         mPresenter.getTypeInfoFromDB(type)
     }
 
+    fun addFinish(boolean: Boolean) {
+        if (boolean) {
+            AppToast.toastShow(mContainerActivity, "创建成功", 1000)
+            newListDialog.dismiss()
+            mPresenter.getTypeInfoFromDB(type)
+        }
+    }
+
     fun setTitleList(temp: ArrayList<String>) {
         titleList.clear()
         titleList.add(DataConstant.DEFAULT_TYPE_NAME)
         titleList.addAll(temp)
         mAdapter.notifyDataSetChanged()
     }
+
+    private fun verifyListTitle(name: String) {
+        if (name.isEmpty()) {
+            AppToast.toastShow(mContainerActivity, "名字不可为空", 1000)
+            return
+        }
+        mPresenter.addData(type, name)
+    }
+
 
     private fun initView() {
         typeLv.adapter = mAdapter
@@ -98,9 +191,8 @@ View.OnClickListener{
         bgIv.background.level =
                 if (type == YueTingConstant.FRAGMENT_TITLE_TYPE_MUSIC) {
                     listTitleTv.text = "创建的歌单"
-                            YueTingConstant.TITLE_TYPE_MUSIC
-                }
-                else {
+                    YueTingConstant.TITLE_TYPE_MUSIC
+                } else {
                     listTitleTv.text = "创建的书单"
                     YueTingConstant.TITLE_TYPE_BOOK
                 }
